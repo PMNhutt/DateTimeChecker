cc9566e603ee15c88ccc51bdefb0b657c786f0be
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2021-09-18 17:18:12 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace DateTimeChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DateTimeChecker.Properties.Resources._20210321075522_Logo_Đại_học_FPT;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 259);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Month";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(151, 334);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Year";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(236, 259);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 27);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(236, 296);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(189, 27);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(236, 334);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(189, 27);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(364, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label4.Location = new System.Drawing.Point(132, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(391, 49);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Date Time Checker";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(664, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+  
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dialog = MessageBox.Show("Are you sure to exit?", "Confirm", MessageBoxButtons.YesNo);
+            if (dialog == DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+            else if (dialog == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }      
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        } 
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //set
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte x,y; short z; 
+            //check textbox có null hay có phải là số hay không?
+            Boolean isCorrectFormat()
+            {
+            
+            if (string.IsNullOrEmpty(textBox2.Text) || !byte.TryParse(textBox2.Text, out x))
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Day is incorrect format!", 
+                    "Error", MessageBoxButtons.OK);
+                    return false;
+            }
+            else if (string.IsNullOrEmpty(textBox3.Text) || !byte.TryParse(textBox3.Text, out y))
+                {
+                    DialogResult dialog = MessageBox.Show("Input data for Month is incorrect format!",
+                    "Error", MessageBoxButtons.OK);
+                    return false;
+                }
+            else if (string.IsNullOrEmpty(textBox4.Text) || !short.TryParse(textBox4.Text, out z))
+                {
+                    DialogResult dialog = MessageBox.Show("Input data for Year is incorrect format!",
+                    "Error", MessageBoxButtons.OK);
+                    return false;
+                }                                                      
+
+                return true;
+            }
+
+
+            byte.TryParse(textBox2.Text, out x);
+            byte.TryParse(textBox3.Text, out y);
+            short.TryParse(textBox4.Text, out z);
+            //check textbox out of range
+            Boolean isValidRange()
+            {
+
+            if (x < 1 || x > 31)
+            {
+                DialogResult dialog = MessageBox.Show("Input data for Day is out of range!",
+                    "Error", MessageBoxButtons.OK);
+                    return false;
+            }
+            else if (y < 1 || y > 12)
+                {
+                    DialogResult dialog = MessageBox.Show("Input data for Month is out of range!",
+                        "Error", MessageBoxButtons.OK);
+                    return false;
+                }
+            else if (z < 1000 || z > 3000)
+                {
+                    DialogResult dialog = MessageBox.Show("Input data for Year is out of range!",
+                        "Error", MessageBoxButtons.OK);
+                    return false;
+                }
+               return true;
+                
+            }
+
+            while ((isCorrectFormat() == true) && (isValidRange() == true))
+            {
+                //check Day in Month
+
+                byte DayInMonth(short Year, byte Month)
+                {
+                    byte Day = 0;
+                    if (Month == 1 || Month == 3 || Month == 5 || Month == 8 || Month == 7 || Month == 10
+                        || Month == 12)
+                    {
+                        Day = 31;
+                    }
+                    else if (Month == 4 || Month == 6 || Month == 9 || Month == 11)
+                    {
+                        Day = 30;
+                    }
+                    else if (Month == 2)
+                    {
+                        if ((Year % 400) == 0)
+                        {
+                            Day = 29;
+                        }
+                        else if ((Year % 100) == 0)
+                        {
+                            Day = 28;
+                        }
+                        else if ((Year % 4) == 0)
+                        {
+                            Day = 29;
+                        }
+                        else
+                        {
+                            Day = 28;
+                        }
+                    }
+                    return Day;
+                }
+
+                //check valid date
+                Boolean IsValidDate(byte Day, byte Month, short Year)
+                {
+                    if (Month >= 1 && Month <= 12)
+                    {
+                        if (Day >= 1)
+                        {
+                            if (Day <= DayInMonth(Year, Month))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                    return false;
+                }
+
+                if (IsValidDate(x, y, z) == true)
+                {
+                    DialogResult dialog = MessageBox.Show($"{x}/{y}/{z} is correct date time!", "Message",
+                        MessageBoxButtons.OK);
+                    break;
+                }
+                else
+                {
+                    DialogResult dialog = MessageBox.Show($"{x}/{y}/{z} is NOT correct date time!", "Message",
+                        MessageBoxButtons.OK);
+                    break;
+                }
+            }
+                        
+            
+        }
+
+    }
+}
